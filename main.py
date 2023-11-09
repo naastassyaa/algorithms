e280136dc50b81e59f826649f08576ae6d299a41
naasttassyaa <naasttassyaa@gmail.com>
2023-11-09 21:06:13 +0200

    created lab4

--- main.py
+++ main.py
@@ -1,40 +1,107 @@
-class BinaryTree:
-    def __init__(self, value, left=None, right=None, parent=None):
-        self.value = value
-        self.left = left
-        self.right = right
-        self.parent = parent
+import copy
+from queue import Queue
 
 
-def find_successor(tree: BinaryTree, node: BinaryTree) -> BinaryTree:
-    visited = []
-
-    def traverse_in_order(node):
-        if node.left:
-            traverse_in_order(node.left)
-        visited.append(node)
-        if node.right:
-            traverse_in_order(node.right)
-
-    traverse_in_order(tree)
-    pos = visited.index(node)
-    for i in visited[pos:]:
-        if i.value > node.value:
-            return i
+def is_valid(x, y, M, N):
+    return 0 <= x < M and 0 <= y < N
 
 
-root = BinaryTree(10)
-root.left = BinaryTree(5)
-root.right = BinaryTree(15)
-root.left.left = BinaryTree(3)
-root.left.right = BinaryTree(7)
-root.right.right = BinaryTree(20)
-root.right.right.left = BinaryTree(12)
+def replace_adjacent_ones(matrix):
+    M = len(matrix)
+    N = len(matrix[0])
+    matrix_copy = copy.deepcopy(matrix)
 
-node_to_find_successor = root.left
-successor = find_successor(root, node_to_find_successor)
+    def has_adjacent_zero(x, y):
+        for dx in [-1, 0, 1]:
+            for dy in [-1, 0, 1]:
+                if dx == 0 and dy == 0:
+                    continue
+                new_x, new_y = x + dx, y + dy
+                if is_valid(new_x, new_y, M, N) and matrix[new_x][new_y] == 0:
+                    return True
+        return False
 
-if successor:
-    print(successor.value)
-else:
-    print("Немає наступника для даної вершини.")
+    for x in range(M):
+        for y in range(N):
+            if matrix[x][y] == 1 and has_adjacent_zero(x, y):
+                matrix_copy[x][y] = 0
+    return matrix_copy
+
+
+
+def graph_from_matrix(matrix):
+    M, N = len(matrix), len(matrix[0])
+    graph = {}
+
+    for i in range(M):
+        for j in range(N):
+            if matrix[i][j] == 1:
+                neighbors = []
+                for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                    ni, nj = i + dx, j + dy
+                    if is_valid(ni, nj, M, N) and matrix[ni][nj] == 1:
+                        neighbors.append((ni, nj))
+                graph[(i, j)] = neighbors
+    return graph
+
+
+def shortest_path_graph(graph, m):
+    counter = 0
+
+    def bfs(node):
+        queue = Queue()
+        visited = set()
+        parent_map = {}
+
+        queue.put((node, 0))
+        visited.add(node)
+
+        while not queue.empty():
+            nonlocal counter
+            counter += 1
+            current_node, distance = queue.get()
+
+            if current_node[1] == m - 1:
+                path = []
+                while current_node:
+                    path.insert(0, current_node)
+                    current_node = parent_map.get(current_node)
+                return distance, path
+
+            for neighbor in graph[current_node]:
+                if neighbor not in visited:
+                    queue.put((neighbor, distance + 1))
+                    visited.add(neighbor)
+                    parent_map[neighbor] = current_node
+        return -1, []
+
+    min_path_len = float('inf')
+    min_path = []
+
+    for node in graph:
+        if node[1] == 0:
+            path_len, path = bfs(node)
+            if path_len != -1:
+                if path_len < min_path_len:
+                    min_path_len = path_len
+                    min_path = path
+
+        if min_path == m - 1:
+            return min_path_len, counter, min_path
+
+    return (min_path_len, counter, min_path) if min_path != float('inf') else (-1, counter)
+
+
+with open("input.txt", "r") as f:
+    matrix = []
+    for line in f.readlines():
+        row = [int(i) for i in line.split()]
+        matrix.append(row)
+
+matrix = replace_adjacent_ones(matrix)
+graph = graph_from_matrix(matrix)
+res = shortest_path_graph(graph, len(matrix[0]))
+
+with open("output.txt", "w") as f:
+    f.write(str(res))
+
